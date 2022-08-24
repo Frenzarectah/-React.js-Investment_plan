006b9aae0c25ff242d136aa03278c97cdeb8e4ec
Frenzarectah <sorrentino.francesco87@gmail.com>
2022-08-24 17:24:48 +0200

    adding modals

--- src/components/Headline/Headline.js
+++ src/components/Headline/Headline.js
@@ -1,16 +1,18 @@
 import React, { useContext } from "react";
 import { globale } from "../../App";
+import HelpModal from "../Modals/HelpModal";
 
 //component which renders the headline above the app, on the right side
 
 const Headline = () =>{
     const [page,setPage] = useContext(globale);
+    const [open,setOpen] = setState(false);
     return(
         <div className='flex flex-row justify-between w-full'>
             <div className='text-[#A4AEB4]'>STEP {page} OF 3</div>
             <div>Lost or have trouble? <a href="#" className='text-[#2696E8] hover:underline'>Get Help!</a></div>
+            <HelpModal/>
         </div>
     )
 }
-
 export default Headline;
---
+++ src/components/Modals/HelpModal.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './modals.css';
+
+const HelpModal = (props) =>{
+    return(
+        <div id="modal">
+            <div class="header">SUPPORT</div>
+            Contact our Helpdesk! Rapido!
+        </div>
+    )
+}
+export default HelpModal;
