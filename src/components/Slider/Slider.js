9f35c4bd6319bda80f401d6b966a24faf99d0940
Frenzarectah <sorrentino.francesco87@gmail.com>
2022-09-01 16:02:11 +0200

    adjusting double range slider

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -2,14 +2,15 @@ import React, { useState } from "react";
 import './slider.css';
 
 const Slider = () =>{
-    const [valuer, setValuerMin] = useState(2);
-    let valuerMin = "";
+    const [valuerMin, setValuerMin] = useState(1);
+    const [valuerMax,setValuerMax] = useState(5);
     const interval = ["10.000","50.000","100.000","200.000","500.000","1.000.000"];
-    {valuer}
     return(
         <div className="mt-[20px] w-full">
+              <div>{valuerMin}</div>
+              <div>{valuerMax}</div>
               <input id="slider" type="range" min="0" max="5" step="1" className="w-full slider" list="valuers" onChange={(e)=>setValuerMin(interval[e.target.value])} />
-              <input id="slider1" type="range" min="0" max="5" step="1" className="w-full slider" onChange={(e)=>valuerMin = interval[e.target.value])}/>
+              <input id="slider1" type="range" min="0" max="5" step="1" className="w-full slider" onChange={(e)=>setValuerMax(interval[e.target.value])}/>
               <datalist id="valuers" className="flex flex-row justify-between mt-[10px] text-[#35A0EE] montserrat">
                 <option value="0" label="10.000$"></option>
                 <option value="1" label="50.000$"></option>
