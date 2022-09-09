5ec81e3030fe526ced50f2aaee7af76101347955
Frenzarectah <sorrentino.francesco87@gmail.com>
2022-09-09 09:27:18 +0200

    added style and logic to CheckBtn and  renderized correctyl

--- src/components/CheckBtn/CheckBtn.js
+++ src/components/CheckBtn/CheckBtn.js
@@ -3,10 +3,10 @@ import '../CheckBtn/CheckBtn.css';
 
 
 const CheckBtn = (props)=>{
-    let className="";
-    if(props.checked)className+="selected";
+    let classes="";
+    props.checked?classes="selected":classes="unselected";
     return(
-        <div id="checkBox" className={className}>
+        <div id="checkBox" className={classes}>
             <input id={props.num} checked={props.checked} type="checkbox" value={props.descr} onChange={props.funct}/>
             <label>{props.descr}</label>
         </div>
--- src/components/Form2/Form2.js
+++ src/components/Form2/Form2.js
@@ -3,20 +3,26 @@ import CheckBtn from "../CheckBtn/CheckBtn";
 import { useState } from "react";
 const Form2 = () =>{
     const [check,setCheck] =useState(false);
-    const checkList = ["Single Family","Residential Multifamily","Commercial Retail","Commercial Industrial",
-                        "Commercial Hospitality","Commercial Warehousing","Commercial Office","Other"];
+    const checkList1 = ["Single Family","Residential Multifamily","Commercial Retail","Commercial Industrial"]
+    const checkList2 = ["Commercial Hospitality","Commercial Warehousing","Commercial Office","Other"];
     return(
     <>
     <p className="montserrat_alt text-[21px] text-black">What kind of real estate are you interested in?</p>
     <div id="wrapper" className="flex flex-col">
         <div className="flex flex-row">
-            <CheckBtn  num="0" descr={checkList[0]} checked={check} funct={()=>setCheck(!check)}/>
+            {checkList1.map((item) =>
+                <CheckBtn num="0" descr={item} checked={check} funct={()=>setCheck(!check)} />
+            )}
         </div>
         <div className="flex flex-row">
-
+            {checkList2.map((item) =>
+                <CheckBtn num="0" descr={item} checked={check} funct={()=>setCheck(!check)} />
+            )}    
         </div>
     </div>
     </>
 )}
 
 export default Form2;
+
+//<CheckBtn  num="0" descr={checkList1[0]} checked={check} funct={()=>setCheck(!check)}/>
