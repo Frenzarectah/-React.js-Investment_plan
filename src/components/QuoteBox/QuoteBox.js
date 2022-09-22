1d9bf3277ab43138a28c5d288d2470fb7e9fa776
Frenzarectah <sorrentino.francesco87@gmail.com>
2022-09-22 15:56:19 +0200

    adjusting LeftSide

--- src/components/QuoteBox/QuoteBox.js
+++ src/components/QuoteBox/QuoteBox.js
@@ -16,10 +16,10 @@ const QuoteBox = () =>{
     const [page] = useContext(globale); 
     return(
         <>
-            <div className="w-[80%] h-[10%] flex flex-col justify-center px-[20px] pt-[10px] items-start bg-white montserrat text-[#A4AEB4] text-md rounded">
-            <p className="text-sm">{pageInfo[page].cit}</p>
+            <div className="w-[330px] h-[200px] flex flex-col justify-center px-[20px] pt-[20px] items-start bg-white montserrat text-[#A4AEB4] text-md rounded">
+            <p className="text-m">{pageInfo[page].cit}</p>
             <div className="mt-[10px] text-left w-full">
-                <p className="text-black font-semibold montserrat_alt text-sm">{pageInfo[page].name}</p>
+                <p className="text-black font-semibold montserrat_alt text-m">{pageInfo[page].name}</p>
                 <p className="text-sm">{pageInfo[page].role}</p>
                 <p className="flex w-full justify-end"><img alt="up logo" src={UP}/></p>
             </div>
